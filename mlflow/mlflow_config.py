1549ed8450f4ea281abff99157325e322dfc9fbb
Ryan NAIDJI <ryan.naidji@gmail.com>
2025-02-07 01:31:21 -0500

    [MLFLOW-DVC] Ajout du suivi DVC pour les modèles MLflow

--- mlflow/mlflow_config.py
+++ mlflow/mlflow_config.py
@@ -1,9 +1,12 @@
 import mlflow
 
+# Utilisation d'une base SQLite locale pour le tracking
+MLFLOW_DB_PATH = "sqlite:///mlflow/mlflow.db"
+
 # Définir l'URI du backend de tracking
-mlflow.set_tracking_uri("sqlite:///mlflow/mlflow.db")
+mlflow.set_tracking_uri(MLFLOW_DB_PATH)
 
-# Définir le nom de l'expérience
-mlflow.set_experiment("brain_tumor_detection")
+# Définir l'expérience MLflow
+mlflow.set_experiment("BrainTumor_Classification")
 
-print("MLflow est configuré avec SQLite.")
+print(f"MLflow configuré avec {MLFLOW_DB_PATH}")
